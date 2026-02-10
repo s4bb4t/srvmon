fd3566e4cc14aac33581c0df94c5158766fbb89a
s4bb4t <s4bb4t@icloud.com>
2026-02-10 19:04:35 +0300

    initial commit

---
+++ checks.go
@@ -0,0 +1,78 @@
+package srvmon
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	pb "github.com/s4bb4t/srvmon/pkg/grpc/srvmon/v1"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (m *SrvMon) Health(ctx context.Context, _ *pb.HealthRequest) (*pb.HealthResponse, error) {
+	resp := &pb.HealthResponse{
+		Version: m.version,
+	}
+
+	var once sync.Once
+	for _, dep := range m.dependencies {
+		check, err := dep.Check(ctx)
+		if err != nil {
+			m.log.Error("dependency check", zap.Error(err))
+			return nil, fmt.Errorf("dependency check: %w", err)
+		}
+
+		resp.Checks = append(resp.Checks, check)
+
+		if check.Status != pb.Status_STATUS_DOWN {
+			continue
+		}
+
+		if dep.MustOK(ctx) {
+			once.Do(func() {
+				resp.Status = pb.Status_STATUS_DOWN
+			})
+		} else {
+			once.Do(func() {
+				resp.Status = pb.Status_STATUS_DEGRADED
+			})
+		}
+	}
+
+	resp.Timestamp = timestamppb.New(time.Now())
+
+	return resp, nil
+}
+
+func (m *SrvMon) Ready(ctx context.Context, _ *pb.ReadinessRequest) (*pb.ReadinessResponse, error) {
+	resp := &pb.ReadinessResponse{
+		Ready:  false,
+		Reason: "",
+	}
+
+	var once sync.Once
+	for _, dep := range m.dependencies {
+		check, err := dep.Check(ctx)
+		if err != nil {
+			m.log.Error("dependency check", zap.Error(err))
+			return nil, fmt.Errorf("dependency check: %w", err)
+		}
+
+		resp.Checks = append(resp.Checks, check)
+		if check.Status != pb.Status_STATUS_DOWN {
+			continue
+		}
+
+		if dep.MustOK(ctx) {
+			once.Do(func() {
+				resp.Ready = false
+				resp.Reason = check.Message
+			})
+		}
+	}
+
+	resp.Timestamp = timestamppb.New(time.Now())
+	return resp, nil
+}
